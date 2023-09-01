4cb6a826b8dbe9f3a96631df76288f983bde6bb6
Serhii Yevchishen <magodza06@gmail.com>
2023-09-01 19:16:11 +0300

    add fix camera

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import Toast from "react-native-toast-message";
+import { useFonts } from 'expo-font';
+import { Provider } from 'react-redux';
+import { store } from "./src/redux/store";
+import Main from './src/components/Main/Main';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./src/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./src/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('./src/fonts/Roboto-Bold.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <Main/>
+      <Toast topOffset={40} />
+    </Provider>
+  );
+};
---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEZjsTk-_YScDtIhWI3gj3Zy-_JCtdR1Q",
+  authDomain: "test-hw7.firebaseapp.com",
+  databaseURL:
+    "https://test-hw7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-hw7",
+  storageBucket: "test-hw7.appspot.com",
+  messagingSenderId: "880217337414",
+  appId: "1:880217337414:web:1ac76023c32104339b6447",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
